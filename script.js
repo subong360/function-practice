f556182c272e9ec18c9723ff96d755a9888a5726
subong360 <40824178+subong360@users.noreply.github.com>
2024-06-24 01:16:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+1. // Write a function averageOfTwo(num1, num2) that returns the average of two numbers.
+
+function averageOfTwo(num1, num2) {
+  // your code here...
+}
+
+console.log(averageOfTwo(3, 7)); // => 5
+console.log(averageOfTwo(16, 5)); // => 10.5
+console.log(averageOfTwo(2, 7.5)); 
+
+2. // Write a function plusFive that takes in a number as an argument and returns the sum of that number and 5.
+function plusFive(num) {
+  // your code here...
+}
+
+console.log(plusFive(10));  // => 15
+console.log(plusFive(2));   // => 7
+console.log(plusFive(-8));  // => -3
+
+3. // We have provided a function named isCool. The isCool function will take a string argument and console.log and return a sentence saying that the given string "is cool!". Call the isCool function with the arguments listed below.
+
+function isCool(string) {
+//c ode goes here
+}
+
+isCool("Music") // => prints "Music is cool!"
+isCool("JavaScript") // => prints "JavaScript is cool!"
+isCool("The world") 
+
+4. // Write a function whisper that takes in a string and returns a "whispered" version of that string.
+
+// HINT: try using the .toLowerCase() function on a string to see what it does!
+
+
+
+function whisper(str) {
+  // your code here...
+}
+
+console.log(whisper("Hey Buddy")); // => "...hey buddy..."
+console.log(whisper("YEA! that was fun")); // => "...yea! that was fun..."
+
+5. // Write a function averageOfFour(num1, num2, num3, num4) that takes in four numbers.
+// The function should return the average of all of the numbers.
+
+
+
+function averageOfFour(num1, num2, num3, num4) {
+//code goes here
+}
+
+console.log(averageOfFour(10, 10, 15, 5)); // => 10
+console.log(averageOfFour(3, 10, 11, 4)); // => 7
+console.log(averageOfFour(1, 2, 3, 4)); /
